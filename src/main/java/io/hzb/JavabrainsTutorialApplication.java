50491a27d1c0c2e41e97ac59d4526d614d9dfe9e
akki19hzb <getakash.verma19@gmail.com>
2018-07-08 19:44:42 +0530

    07082018 - First Commit

---
+++ src/main/java/io/hzb/JavabrainsTutorialApplication.java
@@ -0,0 +1,12 @@
+package io.hzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabrainsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabrainsTutorialApplication.class, args);
+	}
+}
